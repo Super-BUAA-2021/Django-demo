66ce67291460cb2eadde8e21d3cdd5ef2c8610e1
zhtjtcz <18377221@buaa.edu.cn>
2022-01-24 11:13:39 +0800

    Finish URL

--- user/views.py
+++ user/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
-
+import json
+from user.models import User
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
+
+@csrf_exempt
+def register(request):
+	if request.method == 'POST':
+		print("POST")
+		return JsonResponse({'result': 1, 'message': '注册成功!'})
+		
+		# 检测 POST 类型的请求
+		
+		data_json = json.loads(request.body)
+		# 此时 data_json 已经转为了字典类型
+
+		username = data_json['username']
+		password1 = data_json['password1']
+		password2 = data_json['password2']
+		email = data_json.get('email', '')
+		# 不确定是否有这个参数，使用get
+
+		if password1 != password2:
+			return JsonResponse({'result': 0, 'message': '两次密码不一致!'})
+		# 虽然密码一致性与合法性检测一般是前端判断
+		# 但是后端最好也加上，以防万一
+		
+		user = User(username=username, password=password1, email=email)
+		# id 是自动赋值，不需要指明
+		# post 不赋值会使用默认值0
+		
+		user.save()
+		return JsonResponse({'result': 1, 'message': '注册成功!'})
+		# 正常结束后需要给一个反馈信息

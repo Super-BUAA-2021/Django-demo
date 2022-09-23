2c58f16bbc21e61c9632b5e3e589c7ffc1854e58
BFlameSwift <bflameswift@163.com>
2022-09-23 16:32:30 +0800

    Update run.py

--- run.py
+++ run.py
@@ -7,8 +7,11 @@ import platform
 os.system("python manage.py makemigrations")
 os.system("python manage.py migrate")
 
+ip = "0." * 4
+
 if platform.system() != "Linux":
-	os.system("python manage.py runserver")
+    os.system("python manage.py runserver")
 else:
-	os.system("python manage.py runserver 0.0.0.0:8000 > log.txt & \n")
-	print("The backend is running!")
+    # 重定向输出与重定向错误输出，且log.log不同于log.txt
+    os.system("python manage.py runserver " + ip[:-1] + ":8000 >> log.log 2>&1 & \n")
+    print("The backend is running!")
